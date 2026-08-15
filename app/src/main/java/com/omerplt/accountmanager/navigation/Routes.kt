54a74062e760bb035031ffef493103e498f9af94
Ömer <omerplt.dev@gmail.com>
2026-08-15 17:43:15 +0300

    repo tam senkron - duzeltmeler

--- app/src/main/java/com/omerplt/accountmanager/navigation/Routes.kt
+++
@@ -1,8 +0,0 @@
-package com.omerplt.accountmanager.navigation
-
-object Routes {
-    const val HOME = "home"
-    const val ACCOUNT_LIST = "accounts/{appId}"
-
-    fun accountList(appId: Long) = "accounts/$appId"
-}
