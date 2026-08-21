0f0b21caf3eb280089cb305c636e2604d3df4eb0
Ömer <omerplt.dev@gmail.com>
2026-08-21 23:02:13 +0300

    1.1.1 beta 3

--- app/src/main/java/com/starrift/starlock/ui/screens/TrashScreen.kt
+++ app/src/main/java/com/starrift/starlock/ui/screens/TrashScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class, androidx.compose.foundation.ExperimentalFoundationApi::class)
+
 package com.starrift.starlock.ui.screens
 
 import androidx.compose.foundation.background
